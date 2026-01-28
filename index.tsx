b3d7635858a66e743d40b372f4229d30cea4029e
ZeinabAndPixel <villasanazeinab@gmail.com>
2026-01-28 15:18:44 -0400

    Agregado archivo de verificacion Google

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+<meta name="description" content="Z-One Laptop Store: Tienda especializada en laptops y componentes de PC en Venezuela."></meta>
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
